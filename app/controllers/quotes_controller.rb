03f93a7ea54fb348121ff44c8a09ed5ddd0f453d
Joslyn <jrosbrook@gmail.com>
2014-02-16 20:55:18 -0800

    added routing

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,6 +1,8 @@
 class QuotesController < Rulers::Controller
-  def a_quote
-    "There is nothing either good or bad " +
-      "but thinking makes it so."
-  end
+ 
+  def a_quote
+    "There is nothing either good or bad " +
+    "but thinking makes it so." 
+  end
+
 end
